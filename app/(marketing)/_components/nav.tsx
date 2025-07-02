41daca9a5df70f24548c3ae7d59777e3514ac2eb
w-michelle <89858197+w-michelle@users.noreply.github.com>
2025-07-02 13:58:36 -0400

    fix logo

--- app/(marketing)/_components/nav.tsx
+++ app/(marketing)/_components/nav.tsx
@@ -15,7 +15,7 @@ const Nav: React.FC<NavProps> = ({ currentUser }) => {
   const modal = useModal();
 
   return (
-    <div className="flex justify-between items-center py-2 px-6 ">
+    <div className="flex justify-between items-center py-2 px-2">
       <Logo />
 
       <div className="flex items-end gap-4">
