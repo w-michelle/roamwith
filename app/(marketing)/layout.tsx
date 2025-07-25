48ce644afa63020293c9ed8df9c7fdf791d3725e
w-michelle <89858197+w-michelle@users.noreply.github.com>
2025-07-25 16:20:46 -0400

    fix marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,18 +5,20 @@ import Image from "next/image";
 const MarketingLayout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
   return (
-    <div className="h-screen relative bg-gradient-to-b from-sky-50 via-orange-100 to-lime-100 z-[-99]">
-      <Nav currentUser={currentUser} />
-      <main className="pt-40">{children}</main>
-      <div className="absolute top-0 left-0 w-full h-full z-[-99] opacity-20 overflow-hidden">
+    <div className="h-screen relative bg-gradient-to-b from-sky-50 via-orange-100 to-lime-100 ">
+      <div className="absolute top-0 left-0 w-full h-full z-[2] opacity-20 overflow-hidden">
         <Image
           src="/hero.png"
           alt="travelling graphic"
           fill
-          className="object-cover"
+          className="object-contain"
         />
       </div>
-      <Footer />
+      <div className="relative z-10">
+        <Nav currentUser={currentUser} />
+        <main className="pt-40">{children}</main>
+        <Footer />
+      </div>
     </div>
   );
 };
